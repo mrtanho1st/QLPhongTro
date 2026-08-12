60f54dc42e7605e8160a739b55e9a3679e6ddcde
mrtanho1st <tan2005tg@gmail.c>
2026-08-12 20:39:24 +0700

    chinh sua giao dien xem chi tiet

--- QLyPhongTroBackend/src/main/java/com/minhtan/qlptbackend/config/WebConfig.java
+++ QLyPhongTroBackend/src/main/java/com/minhtan/qlptbackend/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:5173",
                         "http://127.0.0.1:5173",
-                        "http://192.168.1.243:5173",
+                        "http://10.10.26.203:5173",
                         "http://localhost:4173",
                         "http://127.0.0.1:4173",
                         "http://localhost:8080")
